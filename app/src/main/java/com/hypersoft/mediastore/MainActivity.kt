fea78adcb4eeebecfe0f385542c9100349d2e005
Yaqoob <social.yaqoob@gmail.com>
2023-08-30 17:39:18 +0500

    MediaStore

--- app/src/main/java/com/hypersoft/mediastore/MainActivity.kt
+++ app/src/main/java/com/hypersoft/mediastore/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.hypersoft.mediastore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hypersoft.mediastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnImages.setOnClickListener {
+
+        }
+
+        binding.btnVideos.setOnClickListener {
+
+        }
+
+        binding.btnAudios.setOnClickListener {
+
+        }
     }
 }
